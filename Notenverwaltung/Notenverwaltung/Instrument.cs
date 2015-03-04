371553cc188c56c7d38e393c4a4971c91afeb751
Martinshorn <martin.salfer@tennet.eu>
2015-03-04 21:20:14 +0100

    Equals-Methodik hinzugefügt

--- Notenverwaltung/Notenverwaltung/Instrument.cs
+++ Notenverwaltung/Notenverwaltung/Instrument.cs
@@ -4,9 +4,9 @@ using System.Windows;
 namespace Notenverwaltung
 {
     /// <summary>
-    /// Stellt ein Instrument dar.
+    /// Stellt ein Instrument (Stimme) dar.
     /// </summary>
-    public class Instrument
+    public class Instrument : IEquatable <Instrument>
     {
         public string Name;
 
@@ -14,8 +14,93 @@ namespace Notenverwaltung
 
         public string Tune;
 
+
+        #region "überschriebene Methode"
         /// <summary>
-        /// ToString zu Testzwecken.
+        /// Gleichheitsprüfung über Objekte
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+           if (obj == null)
+                {
+                    return false;
+                }
+                    
+            Instrument i = obj as Instrument ;
+            if ((System.Object)i == null)
+            {
+                return false;
+            }
+
+            if (i.Name == this.Name && (i.Num == this.Num | i.Num == 0 | this.Num == 0) && (i.Tune == this.Tune | i.Tune == "" | this.Tune == ""))
+                {
+                    return true ;
+                }
+            else
+                return false ;
+        }
+
+        /// <summary>
+        /// Gleichheitsprüfung mit typisiertem Objekt
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public bool Equals(Instrument i)
+        {
+            if (i == null)
+            {
+                return false;
+            }
+
+            if (i.Name == this.Name && (i.Num == this.Num | i.Num == 0 | this.Num == 0) && (i.Tune == this.Tune | i.Tune == "" | this.Tune == ""))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Gleichheitsprüfung ==-Operator
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(Instrument a, Instrument b)
+        {
+            if (System.Object.ReferenceEquals(a, b)) // Wenn beide Objekte null oder die gleiche Instanz sind -> true
+            {
+                return true;
+            }
+
+            if (((object)a == null) || ((object)b == null)) // Wenn ein Objekt null ist, aber nicht beide -> false.
+            {
+                return false;
+            }
+
+            if (a.Name == b.Name && (a.Num == b.Num | a.Num == 0 | b.Num == 0) && (a.Tune == b.Tune | a.Tune == "" | b.Tune == ""))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Gleichheitsprüfung !=-Operator
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(Instrument a, Instrument b)
+        {
+            return !(a == b);
+        }
+
+        /// <summary>
+        /// Ausgabe der Stimmenbezeichnung
         /// </summary>
         public override string ToString()
         {
@@ -26,6 +111,8 @@ namespace Notenverwaltung
             else
                 return String.Format("{2}. {0} in {1}", Name, Tune, Num);
         }
+
+        #endregion
 
         /// <summary>
         /// Erzeugen einer Instanz zu vorliegendem Dateinamen (z.B. "Schlagzeug.pdf")
