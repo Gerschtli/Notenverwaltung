68ad252c6356e06765dd3ad0fc05acdb5cca0d3c
Martinshorn <martin.salfer@tennet.eu>
2015-03-04 21:21:00 +0100

    Besetzungsüberprüfung hinzugefügt

--- Notenverwaltung/Notenverwaltung/Instrumentation.cs
+++ Notenverwaltung/Notenverwaltung/Instrumentation.cs
@@ -10,8 +10,8 @@ namespace Notenverwaltung
     {
         public string Name;
 
-        public List<Instrument> Instruments;
-
+        public List<Instrument> Instruments = new List<Instrument>();
+        
         #region Speicherung
 
         private static readonly string _Path = @"\Einstellungen\Besetzungen.xml";
@@ -33,5 +33,51 @@ namespace Notenverwaltung
         }
 
         #endregion
+
+        #region "öffentliche Funktionen"
+        /// <summary>
+        /// ermittelt die fehlenden Stimmen in der Besetzung
+        /// </summary>
+        /// <param name="b">Besetzung</param>
+        /// <returns></returns>
+        public List<Instrument> GetMissingInstruments(Instrumentation b)
+        {
+            return compareInstrumentation(this.Instruments, b.Instruments);
+        }
+
+        /// <summary>
+        /// ermittelt die überzähligen/überflüssigen Stimmen in der Besetzung
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public List<Instrument> GetNeedlessInstruments(Instrumentation b)
+        {
+            return compareInstrumentation(b.Instruments, this.Instruments);
+        }
+
+        #endregion
+
+        #region "Hilfsfunktionen"
+        /// <summary>
+        /// ermittelt die fehlenden Instrumente
+        /// </summary>
+        /// <param name="a">Besetzungsliste a</param>
+        /// <param name="b">Besetzungsliste b</param>
+        /// <returns></returns>
+        private List<Instrument> compareInstrumentation(List<Instrument> a, List<Instrument> b)
+        {
+            List<Instrument> loInstruments = new List<Instrument>();
+
+            foreach (Instrument i in a)
+            {
+                if (b.Contains(i) == false)
+                {
+                    loInstruments.Add(i);
+                }
+            }
+            return loInstruments;
+        }
+        #endregion
+
     }
 }
