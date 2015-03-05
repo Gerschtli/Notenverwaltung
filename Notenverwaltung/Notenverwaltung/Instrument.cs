dd8d51c3fc03ea044cc55e7a19167d9cdaad8b53
Tobias Happ <tobias.happ@gmx.de>
2015-03-05 19:56:52 +0100

    Umstrukturierung und Formatierung

--- Notenverwaltung/Notenverwaltung/Instrument.cs
+++ Notenverwaltung/Notenverwaltung/Instrument.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Windows;
 
 namespace Notenverwaltung
 {
     /// <summary>
     /// Stellt ein Instrument (Stimme) dar.
     /// </summary>
-    public class Instrument : IEquatable <Instrument>
+    public class Instrument : IEquatable<Instrument>
     {
         public string Name;
 
@@ -14,8 +13,8 @@ namespace Notenverwaltung
 
         public string Tune;
 
+        #region Überschriebene Methoden
 
-        #region "überschriebene Methode"
         /// <summary>
         /// Gleichheitsprüfung über Objekte
         /// </summary>
@@ -23,23 +22,23 @@ namespace Notenverwaltung
         /// <returns></returns>
         public override bool Equals(object obj)
         {
-           if (obj == null)
-                {
-                    return false;
-                }
-                    
-            Instrument i = obj as Instrument ;
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Instrument i = obj as Instrument;
             if ((System.Object)i == null)
             {
                 return false;
             }
 
             if (i.Name == this.Name && (i.Num == this.Num | i.Num == 0 | this.Num == 0) && (i.Tune == this.Tune | i.Tune == "" | this.Tune == ""))
-                {
-                    return true ;
-                }
+            {
+                return true;
+            }
             else
-                return false ;
+                return false;
         }
 
         /// <summary>
@@ -100,6 +99,20 @@ namespace Notenverwaltung
         }
 
         /// <summary>
+        /// Überschreiben der GetHashCode Methode
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            if (Num != 0)
+                return String.Format("{0}#{1}#{2}", Name, Tune, Num).GetHashCode();
+            else if (Tune != null)
+                return String.Format("{0}#{1}", Name, Tune).GetHashCode();
+            else
+                return Name.GetHashCode();
+        }
+
+        /// <summary>
         /// Ausgabe der Stimmenbezeichnung
         /// </summary>
         public override string ToString()
@@ -114,39 +127,70 @@ namespace Notenverwaltung
 
         #endregion
 
+        #region Öffentliche Methoden
+
         /// <summary>
         /// Erzeugen einer Instanz zu vorliegendem Dateinamen (z.B. "Schlagzeug.pdf")
         /// </summary>
         public static Instrument GetInstrument(string filename) // todo: Implementierung von anderen NamePatterns
         {
-            try
+            if (!IsValidFilename(filename))
+                return null;
+
+            filename = filename.TrimEnd(".pdf".ToCharArray());
+
+            string[] result = filename.Split('#');
+
+            Instrument inst = new Instrument();
+
+            switch (result.Length)
             {
-                filename = filename.TrimEnd(".pdf".ToCharArray());
+                case 3:
+                    inst.Num = Convert.ToInt32(result[2]);
+                    goto case 2;
+                case 2:
+                    inst.Tune = result[1];
+                    goto case 1;
+                case 1:
+                    inst.Name = result[0];
+                    break;
+            }
 
-                string[] result = filename.Split('#');
+            return inst;
+        }
 
-                Instrument inst = new Instrument();
+        /// <summary>
+        /// Überprüft, ob der Name ein valider Dateiname ist.
+        /// </summary>
+        public static bool IsValidFilename(string filename)
+        {
+            if (filename == "")
+                return false;
 
-                switch (result.Length)
+            filename = filename.TrimEnd(".pdf".ToCharArray());
+
+            string[] result = filename.Split('#');
+
+            if (result.Length == 3)
+            {
+                try
                 {
-                    case 3:
-                        inst.Num = Convert.ToInt32(result[2]);
-                        goto case 2;
-                    case 2:
-                        inst.Tune = result[1];
-                        goto case 1;
-                    case 1:
-                        inst.Name = result[0];
-                        return inst;
-                    default:
-                        throw new Exception();
+                    Convert.ToInt32(result[2]);
+                    return true;
+                }
+                catch
+                {
+                    return false;
                 }
             }
-            catch
-            {
-                MessageBox.Show("Dateiname ist ungültig.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
-                return default(Instrument);
-            }
+
+            if (result.Length <= 3)
+                return true;
+
+            return false;
         }
+
+        #endregion
+
     }
 }
