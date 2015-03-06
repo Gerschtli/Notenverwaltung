45995e60634de4a1dd30c330ab7c2db34f747b0e
Tobias Happ <tobias.happ@gmx.de>
2015-03-06 03:34:35 +0100

    Dictionary durch SerializableDictionary ersetzt
    Dictionary kann nicht durch den XmlSerializer serialisiert werden, daher
    wird eine Wrapperklasse benötigt.

--- Notenverwaltung/Notenverwaltung/Folder.cs
+++ Notenverwaltung/Notenverwaltung/Folder.cs
@@ -10,7 +10,7 @@ namespace Notenverwaltung
     {
         public string Name;
 
-        public Dictionary<int, string> Order = new Dictionary<int,string>(); // todo: Überprüfen, ob Musikstück existiert (Wertebereich i eingrenzen!)
+        public SerializableDictionary<int, string> Order = new SerializableDictionary<int, string>(); // todo: Überprüfen, ob Musikstück existiert (Wertebereich i eingrenzen!)
 
         #region Speicherung
 
